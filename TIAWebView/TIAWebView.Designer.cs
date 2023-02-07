56f2e1237eeb295d3d61718f2f1557f49bc74d4c
Clément Hamada <Clement.Hamada@i-mation.de>
2023-02-07 10:53:12 +0100

    Initial commit

---
+++ TIAWebView/TIAWebView.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace TIAWebView
+{
+    partial class TIAWebView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.ActivateBrowserOnCreation = false;
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(800, 450);
+            this.webBrowser.TabIndex = 0;
+            // 
+            // TIAWebView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.webBrowser);
+            this.Name = "TIAWebView";
+            this.Size = new System.Drawing.Size(800, 450);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CefSharp.WinForms.ChromiumWebBrowser webBrowser;
+    }
+}
---
+++ TIAWebView/TIAWebView.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Forms;
+
+namespace TIAWebView
+{
+    public partial class TIAWebView : UserControl
+    {
+        private string m_source;
+
+        public string Source
+        {
+            set
+            {
+                bool success = false;
+                try
+                {
+                    webBrowser.LoadUrl(value);
+                    success = true;
+                }
+                catch
+                {
+                
+                }
+                
+                if (success)
+                {
+                    m_source = value;
+                }
+            }
+            get
+            {
+                return m_source;
+            }
+        }
+        
+        public TIAWebView()
+        {
+            InitializeComponent();
+
+            if (Source != null)
+            {
+                webBrowser.LoadUrl(Source);
+            }
+        }
+    }
+}
